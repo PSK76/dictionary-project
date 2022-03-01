f9bc412382c906466b69d849f001950a5bfacee7
Priyanka <prettypri70@gmail.com>
2022-03-01 11:50:40 -0700

    Changed the default keyword

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
           />
         </header>
         <main>
-          <Dictionary defaultKeyword="Technology" />
+          <Dictionary defaultKeyword="music" />
         </main>
         <footer className="App-footer">
           <small>
