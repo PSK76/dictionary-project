b7c45d1724cbdc7393f3a02230fa9759e68698ca
Priyanka <prettypri70@gmail.com>
2022-03-09 20:32:56 -0700

    Updated the Circle play icon

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,13 +1,30 @@
 import React from "react";
-import "./Phonetics.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
 export default function Phonetics(props) {
-  return (
-    <div className="Phonetics">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <span className="text">{props.phonetic.text}</span>
-    </div>
-  );
+  let audio = new Audio(props.phonetic.audio);
+  const start = () => {
+    audio.play();
+  };
+
+  if (props.phonetic.audio) {
+    return (
+      <div className="Phonetics">
+        <div className="row mb-2">
+          <div className="col-1">
+            <a href={Audio} onClick={start}>
+              <FontAwesomeIcon
+                icon={faCirclePlay}
+                className="pt-1 ps-2 button fa-xl"
+              />
+            </a>
+          </div>
+          <div className="col pt-1 ps-2 text">{props.phonetic.text}</div>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
